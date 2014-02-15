d05622e85c1433a769371f6f53d14e77cc8ca155
Alesom Zorzi <alesom.zorzi@gmail.com>
2014-02-15 00:42:32 -0200

    Projeto incial Tem duas telas. Uma de "boas vindas" outra onde mostra a localização

---
+++ Eclipse/Nelb/src/br/edu/uffs/cc/Nelb/BuscaLocalizacao.java
@@ -0,0 +1,51 @@
+package br.edu.uffs.cc.Nelb;
+
+//importação das classes necessárias para o funcionamento do aplicativo
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class BuscaLocalizacao extends Activity {
+
+  //variáveis que usaremos durante o processo
+  private EditText edLatitude;
+  private EditText edLongitude;
+
+  @Override // Método onCreate, iniciada quando o aplicativo é aberto
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.busca_localizacao);
+      edLatitude = (EditText) findViewById(R.id.edLatitude);
+      edLongitude = (EditText) findViewById(R.id.edLongitude);
+      startGPS();
+  }
+
+  //Método que faz a leitura de fato dos valores recebidos do GPS
+  public void startGPS(){
+      LocationManager lManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+      LocationListener lListener = new LocationListener() {
+          public void onLocationChanged(Location locat) {
+              updateView(locat);
+          }
+          public void onStatusChanged(String provider, int status, Bundle extras) {}
+          public void onProviderEnabled(String provider) {}
+          public void onProviderDisabled(String provider) {}
+      };
+      lManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, lListener);
+  }
+
+  //  Método que faz a atualização da tela para o usuário.
+  public void updateView(Location locat){
+      Double latitude = locat.getLatitude();
+      Double longitude = locat.getLongitude();
+
+      edLatitude.setText(latitude.toString());
+      edLongitude.setText(longitude.toString());
+  }
+}
